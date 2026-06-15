ba0c55fd51fc1c83ab91d1b151cafcd7d50d05b2
Federico <federicomasucci17@gmail.com>
2026-06-15 12:32:30 +0200

    new file:   esercizio-4/README.md 	new file:   esercizio-4/index.html 	new file:   esercizio-4/main.js

---
+++ esercizio-4/README.md
@@ -0,0 +1,11 @@
+# Esercizio 4
+
+Descrivi le informazioni essenziali di un libro
+
+- Titolo
+- Sub-Titolo
+- Autore
+- Casa Editrice
+- Anno di pubblicazione
+- ISBN
+- genere
---
+++ esercizio-4/main.js
@@ -0,0 +1,9 @@
+const libro = {
+    titolo: "L'arte della Guerra",
+    subtitolo: "Non Presenta sub-titolo",
+    autore: "Sun Tzu",
+    casaEdi: "Mondadori",
+    annoPub: "2003",
+    ISBN: "978-8804517788",
+    genere: "Saggistica / Trattato di strategia militare e filosofia orientale"
+}
