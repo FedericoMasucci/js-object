f8f5f3e4e76a6c50b71e3b4a5cc59b4a9f38b331
Federico <federicomasucci17@gmail.com>
2026-06-15 12:17:51 +0200

    new file:   esercizio-3/README.md 	new file:   esercizio-3/index.html 	new file:   esercizio-3/main.js

---
+++ esercizio-3/README.md
@@ -0,0 +1,10 @@
+# Esercizio 3
+
+Descrivi le specifiche tecniche di un elettrodomestico
+
+- modello
+- scalaAG
+- misureEsterne
+- misucraCarico
+- rumorosita
+- potenza
---
+++ esercizio-3/main.js
@@ -0,0 +1,8 @@
+const elettrodomestico = {
+    modello: " Lavatrice TechWash Eco 900",
+    scalaAG: "A",
+    misureEsterne: [85, 60, 60],
+    misuraCarico: "9 kg",
+    rumorosita: "71 db",
+    potenza: "2220 Watt"
+}
